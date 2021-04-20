2465b74b974f032368ec3481a49f50a5670713f9
Anton <tonatoz@gmail.com>
2021-04-20 12:18:49 +0300

    fast but lot of false positive

--- src/task1.py
+++ src/task1.py
@@ -1,9 +1,12 @@
 from aiohttp import ClientSession
 from asyncio import get_event_loop, create_task, gather
 from loguru import logger
-from bs4 import BeautifulSoup
 import re
 import time
+
+phone_pattern = re.compile(
+    "((\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2})"
+)
 
 
 async def fetch(session: ClientSession, url: str) -> str:
@@ -11,32 +14,29 @@ async def fetch(session: ClientSession, url: str) -> str:
     return await resp.text()
 
 
-async def parse(html: str) -> list[str]:
-    soup = BeautifulSoup(html, "html.parser")
-    texts = soup.findAll(text=True)
-    return [t.strip() for t in texts]
-
-
-def find_phone_nubmer(text):
-    phone = re.match(
-        "^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$",
+def find_phone_numbers(text):
+    phones = re.findall(
+        phone_pattern,
         text,
     )
-    if phone:
-        clean_phone = list(re.sub("[^0-9]", "", phone.group()))
-        clean_phone[0] = "8"  # if +7 - change to 8, if 8 - it's good
-        return "".join(clean_phone)
+    for [phone, _] in phones:
+        clean_phone = list(re.sub("[^0-9]", "", phone))
+        if len(clean_phone) == 10:
+            clean_phone.insert(0, "8")
+        elif len(clean_phone) == 7:
+            clean_phone = ["8", "4", "9", "5"] + clean_phone
+        elif clean_phone[0] == "7":
+            clean_phone[0] = "8"
+        yield "".join(clean_phone)
 
 
 async def process(session: ClientSession, url: str) -> tuple[str, list[str]]:
     try:
         html = await fetch(session, url)
-        texts = await parse(html)
-        phones = [find_phone_nubmer(t) for t in texts]
-        return url, list(filter(lambda x: x, phones))
+        return url, [x for x in find_phone_numbers(html)]
     except Exception as e:
-        logger.warning(e)
-        return []
+        logger.error(e)
+        return url, []
 
 
 async def main(urls: list[str]):
@@ -44,14 +44,14 @@ async def main(urls: list[str]):
         start = time.time()
         tasks = [create_task(process(session, url)) for url in urls]
         results = await gather(*tasks)
-        # logger.info(f"Result: {results}")
+        logger.info(f"Result: {results}")
         logger.info(f"Time elapsed: {time.time() - start} ")
 
 
 if __name__ == "__main__":
     loop = get_event_loop()
     try:
-        organizations = ["https://masterdel.ru/", "https://repetitors.info/"] * 100
+        organizations = ["https://masterdel.ru/", "https://repetitors.info/"]
         loop.run_until_complete(main(organizations))
     except:
         logger.exception("Error")
