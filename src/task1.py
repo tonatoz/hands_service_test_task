1e30d73e979e95c740c61c891da641c02ed0a8ab
Anton <tonatoz@gmail.com>
2021-04-20 13:11:07 +0300

    using lxml for parsing

--- src/task1.py
+++ src/task1.py
@@ -1,11 +1,12 @@
+import re
 from aiohttp import ClientSession
 from asyncio import get_event_loop, create_task, gather
+from bs4 import BeautifulSoup
 from loguru import logger
-import re
-import time
+
 
 phone_pattern = re.compile(
-    "((\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2})"
+    r"^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$"
 )
 
 
@@ -14,26 +15,22 @@ async def fetch(session: ClientSession, url: str) -> str:
     return await resp.text()
 
 
-def find_phone_numbers(text):
-    phones = re.findall(
-        phone_pattern,
-        text,
-    )
-    for [phone, _] in phones:
-        clean_phone = list(re.sub("[^0-9]", "", phone))
-        if len(clean_phone) == 10:
-            clean_phone.insert(0, "8")
-        elif len(clean_phone) == 7:
-            clean_phone = ["8", "4", "9", "5"] + clean_phone
-        elif clean_phone[0] == "7":
-            clean_phone[0] = "8"
-        yield "".join(clean_phone)
+def fix_format(text):
+    clean_phone = list(re.sub(r"[^0-9]", "", text))
+    if len(clean_phone) == 10:
+        clean_phone.insert(0, "8")
+    elif len(clean_phone) == 7:
+        clean_phone = ["8", "4", "9", "5"] + clean_phone
+    elif clean_phone[0] == "7":
+        clean_phone[0] = "8"
+    return "".join(clean_phone)
 
 
 async def process(session: ClientSession, url: str) -> tuple[str, list[str]]:
     try:
         html = await fetch(session, url)
-        return url, [x for x in find_phone_numbers(html)]
+        texts = BeautifulSoup(html, "lxml").findAll(text=phone_pattern)
+        return url, [fix_format(t) for t in texts]
     except Exception as e:
         logger.error(e)
         return url, []
@@ -41,17 +38,19 @@ async def process(session: ClientSession, url: str) -> tuple[str, list[str]]:
 
 async def main(urls: list[str]):
     async with ClientSession() as session:
-        start = time.time()
         tasks = [create_task(process(session, url)) for url in urls]
         results = await gather(*tasks)
-        logger.info(f"Result: {results}")
-        logger.info(f"Time elapsed: {time.time() - start} ")
+        return results
 
 
-if __name__ == "__main__":
+def run(organizations):
     loop = get_event_loop()
     try:
-        organizations = ["https://masterdel.ru/", "https://repetitors.info/"]
         loop.run_until_complete(main(organizations))
     except:
         logger.exception("Error")
+
+
+if __name__ == "__main__":
+    organizations = ["https://masterdel.ru/", "https://repetitors.info/"]
+    run(organizations)
